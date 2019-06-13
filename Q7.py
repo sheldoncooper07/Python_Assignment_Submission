2c17059f9b0fa27b9fd9c785efb0b1bccd1fce48
Piyush <sheldoncoopernit@gmail.com>
2019-06-13 12:25:42 +0530

    Add files via upload

---
+++ Q7.py
@@ -0,0 +1,22 @@
+#Problem7
+
+#!/usr/bin/python3
+
+print('''
+1. Create a single file
+2. Create multiple file
+3. Command similar to -c 
+''')
+option = input('Select an option: ')
+if option == '1':
+	fname=input("Enter file name")
+	f=open(fname+".txt","w+")
+	f.close()
+if option =='2':
+	num=int(input("how many files u wanna create"))
+	for i in range(0,num):
+	 fname=input("Enter file name")
+	 f=open(fname+".txt","w+")
+	 f.close()
+if option =='3':
+	print("You cannot create a file")
